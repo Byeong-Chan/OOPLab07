ec459a8013493f175e32ea30c4aeb5e3f57a0110
seoHyeon <Owner@Owner-PC>
2017-04-27 12:19:01 +0900

    Laptop implements DataStorage

--- ProductManagement/src/seoHyeon/Laptop.java
+++ ProductManagement/src/seoHyeon/Laptop.java
@@ -1,6 +1,9 @@
 package seoHyeon;
 
-public class Laptop implements Networked{
+import byeongChan.Product;
+import k_package.DataStorage;
+
+public class Laptop implements Networked,DataStorage{
 	private double totalCapacity;
 	private double usedCapacity;
 	public Laptop(double tc, double uC){
